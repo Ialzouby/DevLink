756eff5715df4a0a3eec868dbef836118c332640
ialzouby <ialzouby@gmail.com>
2024-10-22 15:53:51 -0400

    test3

--- DevLink/settings.py
+++ DevLink/settings.py
@@ -90,9 +90,7 @@ WSGI_APPLICATION = 'DevLink.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')  # Railway's internal DB URL for production
-    )
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 

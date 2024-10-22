359bbeb378a353163d1b0445a7bc983a8393b7dc
ialzouby <ialzouby@gmail.com>
2024-10-22 17:50:34 -0400

    Update wsgi.py

--- DevLink/wsgi.py
+++ DevLink/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DevLink.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DEVLINK.settings')
 
 application = get_wsgi_application()

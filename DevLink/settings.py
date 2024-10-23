965d46a6a7d3c8de63cb652a3e60a189b99e0cb8
ialzouby <ialzouby@gmail.com>
2024-10-23 19:44:58 -0400

    Imports

--- DevLink/settings.py
+++ DevLink/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.

0b7645d86f07e4dd6ef86cf1ded047bd00177256
ialzouby <ialzouby@gmail.com>
2024-10-23 20:46:21 -0400

    test

--- projects/storage_backends.py
+++
@@ -1,9 +0,0 @@
-from whitenoise.storage import CompressedManifestStaticFilesStorage
-
-class CompressedManifestMediaStorage(CompressedManifestStaticFilesStorage):
-    """
-    Custom storage backend for serving media files using Whitenoise.
-    This is only recommended for small-scale media usage.
-    """
-    def url(self, name, *args, **kwargs):
-        return super().url(name, *args, **kwargs)

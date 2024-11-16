1e1baf75d6ecf7bcd3e2abaf710ed2bfbf73dd3a
ialzouby <ialzouby@gmail.com>
2024-11-16 09:36:22 -0500

    google setup

--- DevLink/settings.py
+++ DevLink/settings.py
@@ -94,7 +94,7 @@ ROOT_URLCONF = 'DevLink.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'allauth'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

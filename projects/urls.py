c7720fb49f9f9a2ef9eedfb80f8ffe994139769f
ialzouby <ialzouby@gmail.com>
2024-11-16 14:57:03 -0500

    settings

--- projects/urls.py
+++ projects/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('profile/<str:username>/', profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='projects/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='projects/logout.html'), name='logout'), 
     path('project/', views.project, name='project-list'),  # URL for listing projects (adjust if needed)
     path('project/<int:project_id>/', views.project, name='project'),  # URL for viewing a specific project

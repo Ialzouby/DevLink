5e55130c0f8c5cf64809cacc5577c7795836cce8
ialzouby <ialzouby@gmail.com>
2024-11-16 14:14:50 -0500

    Update settings.py

--- DevLink/settings.py
+++ DevLink/settings.py
@@ -93,7 +93,7 @@ ROOT_URLCONF = 'DevLink.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'projects/templates')],
+        'DIRS': ['projects/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
